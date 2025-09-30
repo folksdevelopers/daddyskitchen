e75aad45455bf7bbcff2da3a188d5711ac5d016f
Munavvir Mpr <munavvir303@gmail.com>
2025-09-30 20:08:33 +0000

    Changes before Firebase Studio auto-run

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Gilroy', 'serif'],
+        headline: ['Gilroy', 'serif'],
         code: ['monospace'],
       },
       colors: {
