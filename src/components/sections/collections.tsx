b946a3d9e139834cea196adf522346e9d891badd
Media Buying 360 <contact@mediabuying360.com>
2025-10-03 05:06:22 +0000

    make it Our Product Categories (_for element h2_)

--- src/components/sections/collections.tsx
+++ src/components/sections/collections.tsx
@@ -41,7 +41,7 @@ export function Collections() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center mb-12">
             <div className='pb-4'>
                  <h2 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
-                    Best Delivered <br /> <span className='text-primary'>Categories</span>
+                    Our Product <span className='text-primary'>Categories</span>
                 </h2>
             </div>
             <div>
