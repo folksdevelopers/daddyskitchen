a6dedea76f3d7c8449289dbdd56da3853525d744
Munavvir Mpr <munavvir303@gmail.com>
2025-10-05 18:52:21 +0000

    make it align center (_for element div_)

--- src/components/sections/product-details.tsx
+++ src/components/sections/product-details.tsx
@@ -22,9 +22,9 @@ export function ProductDetails({ product }: { product: Product }) {
         <div className="rounded-2xl bg-card shadow-lg overflow-hidden">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-12 items-start">
             
-            <div className="relative h-80 lg:h-[500px] w-full order-first lg:order-last bg-secondary/30 flex items-center justify-center">
+            <div className="relative h-80 lg:h-full p-[10px] w-full order-first lg:order-last bg-secondary/30 flex items-center justify-center">
                 <div className='absolute inset-0 w-full h-full bg-primary/10 blur-3xl rounded-full -z-0' />
-              <div className="relative w-full h-full">
+              
                 <Image
                     src={product.imageUrl}
                     alt={product.name}
@@ -32,7 +32,7 @@ export function ProductDetails({ product }: { product: Product }) {
                     fill
                     className="object-cover"
                 />
-              </div>
+             
             </div>
             
             <div className="p-6 sm:p-8 lg:p-12">
